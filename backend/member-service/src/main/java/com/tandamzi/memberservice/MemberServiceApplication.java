0847c14f639ba2a0843bc0be9385b5c0688591a0
ss0takk <ss0takk@gmail.com>
2023-04-19 17:48:45 +0900

    init: member service 생성

---
+++ backend/member-service/src/main/java/com/tandamzi/memberservice/MemberServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tandamzi.memberservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberServiceApplication.class, args);
+	}
+
+}
