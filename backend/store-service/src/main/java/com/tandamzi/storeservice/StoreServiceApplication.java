ce18d81382d255dc068f0bd397af506e8f3e20e0
geon1120@gmail.com <geon1120@gmail.com>
2023-04-20 10:07:10 +0900

    Feat: JPA Auditing어노테이션 추가

--- backend/store-service/src/main/java/com/tandamzi/storeservice/StoreServiceApplication.java
+++ backend/store-service/src/main/java/com/tandamzi/storeservice/StoreServiceApplication.java
@@ -2,8 +2,10 @@ package com.tandamzi.storeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class StoreServiceApplication {
 
     public static void main(String[] args) {
