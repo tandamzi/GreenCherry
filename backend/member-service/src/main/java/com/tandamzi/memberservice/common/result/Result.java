dea51882b6708702a4f735564736f3e7edf540f1
ss0takk <ss0takk@gmail.com>
2023-04-20 16:37:38 +0900

    feat: response result 생성

---
+++ backend/member-service/src/main/java/com/tandamzi/memberservice/common/result/Result.java
@@ -0,0 +1,11 @@
+package com.tandamzi.memberservice.common.result;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Result {
+    private int code;
+    private String message;
+}
