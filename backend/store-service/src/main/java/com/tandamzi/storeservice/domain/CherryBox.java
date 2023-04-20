00af4b33a61e2380a8910a17c6399200a3d6d16c
geon1120@gmail.com <geon1120@gmail.com>
2023-04-20 14:11:55 +0900

    feat: entity 추가

--- backend/store-service/src/main/java/com/tandamzi/storeservice/domain/CherryBox.java
+++ backend/store-service/src/main/java/com/tandamzi/storeservice/domain/CherryBox.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Builder
@@ -19,4 +16,6 @@ public class CherryBox extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @OneToOne(mappedBy = "cherryBox", fetch = FetchType.LAZY)
+    private Store store;
 }
