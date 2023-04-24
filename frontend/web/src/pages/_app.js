f6e70be35e22622be1070b55a15eb270dbc31758
DESKTOP-KVCQHCD\multicampus <codms1104@naver.com>
2023-04-24 14:53:59 +0900

    Init: web프로젝트 pwa 설정

---
+++ frontend/web/src/pages/_app.js
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+
+function App({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then((registration) => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch((error) => {
+          console.error('Service Worker registration failed:', error);
+        });
+    }
+  }, []);
+
+  return <Component {...pageProps} />;
+}
+
+export default App;
