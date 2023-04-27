423b2dc681950dac6802797b74611c346d0c089a
pthwan27 <parkssuu@gmail.com>
2023-04-27 10:54:01 +0900

    chore :mobile husky test

--- frontend/mobile/src/pages/api/send-notification.js
+++ frontend/mobile/src/pages/api/send-notification.js
@@ -1,43 +1,43 @@
-import webPush from "web-push";
-import { getSubscriptions } from "../../utils/db";
+import webPush from 'web-push';
+import { getSubscriptions } from '../../utils/db';
 
 const publicVapidKey = process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY;
 const privateVapidKey = process.env.VAPID_PRIVATE_KEY;
 
 webPush.setVapidDetails(
-  "mailto:rachaenlee@gmail.com",
+  'mailto:rachaenlee@gmail.com',
   publicVapidKey,
-  privateVapidKey
+  privateVapidKey,
 );
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     const subscriptions = getSubscriptions();
     console.log(subscriptions);
     const notificationPayload = {
-      title: "Hello from PWA",
-      body: "This is a test push notification",
-      icon: "/icon-192x192.png",
-      badge: "/icon-192x192.png",
+      title: 'Hello from PWA',
+      body: 'This is a test push notification',
+      icon: '/icon-192x192.png',
+      badge: '/icon-192x192.png',
     };
 
     try {
       console.log(notificationPayload);
-      console.log("ㄱㄱㄱ?");
+      console.log('ㄱㄱㄱ?');
       for (const subscription of subscriptions) {
-        console.log("여기엔 도달하니?", subscription);
+        console.log('여기엔 도달하니?', subscription);
         await webPush.sendNotification(
           subscription,
-          JSON.stringify(notificationPayload)
+          JSON.stringify(notificationPayload),
         );
       }
-      res.status(200).json({ message: "Push notifications sent" });
+      res.status(200).json({ message: 'Push notifications sent' });
     } catch (error) {
       console.error(error);
       console.log(subscriptions);
-      res.status(500).json({ message: "Failed to send push notifications" });
+      res.status(500).json({ message: 'Failed to send push notifications' });
     }
   } else {
-    res.status(405).json({ message: "Method not allowed" });
+    res.status(405).json({ message: 'Method not allowed' });
   }
 }
