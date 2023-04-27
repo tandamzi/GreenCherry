b89e484023efae29c177a691e2316db6a859fb67
geonhyeon <geon1120@gmail.com>
2023-04-27 14:35:49 +0900

    refactor: add comment

--- backend/store-service/src/main/java/com/tandamzi/storeservice/service/S3Service.java
+++ backend/store-service/src/main/java/com/tandamzi/storeservice/service/S3Service.java
@@ -24,6 +24,11 @@ public class S3Service {
     private String bucket;
     private final AmazonS3Client amazonS3Client;
 
+    /*
+    dirName에 이미지 파일의 저장될 위치 지정(예: "store")
+    파일 배열 각각 업로드 후 업로드된 파일의 URL을 리스트에 저장 후 반환
+    controller에서 @RequestPart로 List<MultiPartFile>을 받아오기. @RequestBody안됩니다.
+     */
     public List<String> uploadFiles(List<MultipartFile> multipartFileList, String dirName) throws IOException {
         List<String> imageUrlList = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
@@ -34,7 +39,7 @@ public class S3Service {
             sb.append(UUID.randomUUID());
             sb.append(multipartFile.getOriginalFilename());
             String storedFilePath = sb.toString();
-            long size = multipartFile.getSize(); // 파일 크기
+            long size = multipartFile.getSize();
             ObjectMetadata objectMetaData = new ObjectMetadata();
             objectMetaData.setContentType(multipartFile.getContentType());
             objectMetaData.setContentLength(size);
@@ -42,7 +47,7 @@ public class S3Service {
                     new PutObjectRequest(bucket, storedFilePath, multipartFile.getInputStream(), objectMetaData)
                             .withCannedAcl(CannedAccessControlList.PublicRead)
             );
-            String imagePath = amazonS3Client.getUrl(bucket, storedFilePath).toString(); // 접근가능한 URL 가져오기
+            String imagePath = amazonS3Client.getUrl(bucket, storedFilePath).toString();
             imageUrlList.add(imagePath);
         }
         return imageUrlList;
