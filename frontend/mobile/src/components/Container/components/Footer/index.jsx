01c07efa0a25e357bc32a11a0efee1556eff0ecb
pthwan27 <parkssuu@gmail.com>
2023-04-28 01:01:05 +0900

    Chore: image,icon 추가, mobile container구조 추가

---
+++ frontend/mobile/src/components/Container/components/Footer/index.jsx
@@ -0,0 +1,40 @@
+import React, { useRef, useEffect } from 'react';
+
+import useResizeObserver from '@react-hook/resize-observer';
+import cs from 'classnames';
+
+const useSizeHeight = target => {
+  const [size, setSize] = React.useState();
+
+  React.useLayoutEffect(() => {
+    setSize(target.current.getBoundingClientRect().height);
+  }, [target]);
+
+  useResizeObserver(target, entry => setSize(entry.contentRect.height));
+
+  return size;
+};
+const Footer = ({ className, children }) => {
+  const footerRef = useRef();
+  const height = useSizeHeight(footerRef);
+
+  // cherry-body에 footer의 높이만큼 주되 32px 정도를 여분으로 더 줍니다.
+  useEffect(() => {
+    const bodyRef = document.getElementById('cherry-body');
+    if (bodyRef) {
+      bodyRef.style.marginBottom = `${height + 32}px`;
+    }
+  }, [height]);
+
+  return (
+    <div
+      ref={footerRef}
+      id="cherry-footer"
+      className={cs('fixed bottom-0 m-0 w-full bg-white', className)}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Footer;
