8c7d3a6a579c352422f6867ab1b40ecde84f6c29
pthwan27 <parkssuu@gmail.com>
2023-04-27 10:43:37 +0900

    chore: husky test

--- frontend/web/src/components/Spinner.jsx
+++ frontend/web/src/components/Spinner.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Spinner = ({ content }) => {
+function Spinner({ content }) {
   return (
     <div>
       <p className="text-center font-bold text-4xl mt-20">{content}</p>
     </div>
   );
-};
+}
 
 Spinner.defaultProps = {
   content: '로딩중.....♡',
