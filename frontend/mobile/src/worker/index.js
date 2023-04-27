9db82751f71b798cd051db6b3846e5d5ffbfadea
pthwan27 <parkssuu@gmail.com>
2023-04-27 11:39:15 +0900

    chore :mobile husky test

--- frontend/mobile/src/worker/index.js
+++ frontend/mobile/src/worker/index.js
@@ -1,15 +1,13 @@
 // 서비스 워커에게 푸시 이벤트를 수신하도록 지시
 /* eslint-disable no-console */
-
 self.addEventListener('push', event => {
   console.log('[Service Worker] Push Received.', event.data.text());
   const { title, body } = event.data.json();
   event.waitUntil(self.registration.showNotification(title, { body }));
 });
 
-self.addEventListener('notificationclick', event => {
+self.addEventListener('notificationclick', () => {
   console.log('[Service Worker] notificationclick');
-  clients.openWindow(event.notification.data.link);
 });
 
 self.addEventListener('install', () => {
