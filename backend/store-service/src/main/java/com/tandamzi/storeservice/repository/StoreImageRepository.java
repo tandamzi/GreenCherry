3afa9e2f88bef749b5849a075b7e8e41d5da1e6a
geonhyeon <geon1120@gmail.com>
2023-04-27 17:41:13 +0900

    feat: StoreImage Update

--- backend/store-service/src/main/java/com/tandamzi/storeservice/repository/StoreImageRepository.java
+++ backend/store-service/src/main/java/com/tandamzi/storeservice/repository/StoreImageRepository.java
@@ -3,6 +3,7 @@ package com.tandamzi.storeservice.repository;
 import com.tandamzi.storeservice.domain.Store;
 import com.tandamzi.storeservice.domain.StoreImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,7 @@ import java.util.List;
 @Repository
 public interface StoreImageRepository extends JpaRepository<StoreImage, Long> {
     List<StoreImage> findStoreImagesByStore(Store store);
+
+    void deleteStoreImagesByStore(Store store);
+
 }
