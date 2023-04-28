0941a6cc301dbc17a0cf401e0a26b6fa4bfd24eb
이채은 <codms1104@naver.com>
2023-04-28 23:43:40 +0900

    design: user business toggle 위치

--- frontend/web/src/components/Container/MainHeader/index.jsx
+++ frontend/web/src/components/Container/MainHeader/index.jsx
@@ -6,11 +6,11 @@ import style from './index.module.scss';
 import Header from '@/components/Container/Header';
 import SwitchButton from '@/components/Container/SwitchButton';
 
-const BusinessHeader = ({ children, className }) => {
+const MainHeader = ({ children, className }) => {
   return (
     <Header
       className={classnames(
-        'flex items-center fixed w-full h-24 top-0 z-30 shadow-header rounded-header bg-secondary',
+        'flex justify-between items-center fixed w-full h-24 top-0 z-30 shadow-header rounded-header bg-secondary',
         className,
       )}
     >
@@ -18,13 +18,12 @@ const BusinessHeader = ({ children, className }) => {
         src="/assets/logo/mainLogo-green.svg"
         width={386}
         height={69}
-        className={classnames('tablet:w-3/6 flex-none tablet:ml-0', style.logo)}
+        className={classnames('tablet:w-3/6 tablet:ml-0', style.logo)}
         alt="greencherry main logo"
       />
       <SwitchButton />
-      <div className="flex items-center ml-auto">{children}</div>
     </Header>
   );
 };
 
-export default BusinessHeader;
+export default MainHeader;
