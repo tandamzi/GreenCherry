5c5410dadcedea97d440be680cb770fa6eebef45
pthwan27 <parkssuu@gmail.com>
2023-04-28 10:00:38 +0900

    chore: footer test

--- frontend/mobile/src/components/Container/components/MainFooterWithNavigation/index.jsx
+++ frontend/mobile/src/components/Container/components/MainFooterWithNavigation/index.jsx
@@ -1,11 +1,14 @@
 import { memo } from 'react';
+import { FaMapMarkerAlt, FaHome, FaUserAlt } from 'react-icons/fa';
 import { FiHome, FiCamera, FiUser } from 'react-icons/fi';
 
 import cs from 'classnames';
 import Link from 'next/link';
+import { Container } from 'postcss';
 
 import style from './index.module.scss';
 import MainFooter from '../MainFooter';
+import MainHeader from '../MainHeader';
 
 const MainFooterWithNavigation = ({ className }) => {
   return (
@@ -16,16 +19,16 @@ const MainFooterWithNavigation = ({ className }) => {
       )}
     >
       <Link href="/" className="flex flex-col w-16 text-center">
-        <FiHome size={32} className="shrink-0 ml-auto mr-auto" />
-        <span className={cs(`font-medium text-xs`)}>홈</span>
+        <FaMapMarkerAlt size={32} className="shrink-0 ml-auto mr-auto" />
+        <span className={cs(`font-medium text-xs`)}>내주변가게</span>
       </Link>
       <Link href="/auth" className="flex flex-col w-16 text-center relative">
         <div className={cs('absolute', style.BackgroundCircle)} />
-        <FiCamera size={38} className="shrink-0 ml-auto mr-auto" />
-        <span className={cs(`font-medium text-sm`)}>인증</span>
+        <FaHome size={38} className="shrink-0 ml-auto mr-auto" />
+        <span className={cs(`font-medium text-sm`)}>홈</span>
       </Link>
       <Link href="/mypage" className="flex flex-col w-16 text-center">
-        <FiUser size={32} className="shrink-0 ml-auto mr-auto" />
+        <FaUserAlt size={32} className="shrink-0 ml-auto mr-auto" />
         <span className={cs(`font-medium text-xs`)}>마이페이지</span>
       </Link>
     </MainFooter>
