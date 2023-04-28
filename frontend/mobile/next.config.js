3054f068e05125825faeaee6278aa2770dfca748
pthwan27 <parkssuu@gmail.com>
2023-04-28 14:09:40 +0900

    feat: footer 수정

--- frontend/mobile/next.config.js
+++ frontend/mobile/next.config.js
@@ -1,15 +1,18 @@
-const runtimeCaching = require("next-pwa/cache");
+const runtimeCaching = require('next-pwa/cache');
 
-const withPWA = require("next-pwa")({
-  dest: "public",
+const withPWA = require('next-pwa')({
+  dest: 'public',
   register: true,
   skipWaiting: true,
-  customWorkerDir: "worker",
+  customWorkerDir: 'worker',
   runtimeCaching,
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withPWA({
+  devIndicators: {
+    autoPrerender: false,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
--- frontend/mobile/src/components/Container/components/MainFooterWithNavigation/index.jsx
+++ frontend/mobile/src/components/Container/components/MainFooterWithNavigation/index.jsx
@@ -1,14 +1,12 @@
 import { memo } from 'react';
-import { FaMapMarkerAlt, FaHome, FaUserAlt } from 'react-icons/fa';
-import { FiHome, FiCamera, FiUser } from 'react-icons/fi';
+import { FaMapMarkerAlt, FaUserAlt } from 'react-icons/fa';
+import { HiHome } from 'react-icons/hi';
 
 import cs from 'classnames';
 import Link from 'next/link';
-import { Container } from 'postcss';
 
 import style from './index.module.scss';
 import MainFooter from '../MainFooter';
-import MainHeader from '../MainHeader';
 
 const MainFooterWithNavigation = ({ className }) => {
   return (
@@ -18,18 +16,18 @@ const MainFooterWithNavigation = ({ className }) => {
         className,
       )}
     >
-      <Link href="/" className="flex flex-col w-16 text-center">
-        <FaMapMarkerAlt size={32} className="shrink-0 ml-auto mr-auto" />
-        <span className={cs(`font-medium text-xs`)}>내주변가게</span>
+      <Link href="/" className="flex flex-col w-17 text-center">
+        <FaMapMarkerAlt size={28} className=" shrink-0 ml-auto mr-auto" />
+        <span className={cs(`text-xs mt-1 font-bold`)}>내 주변 가게</span>
       </Link>
-      <Link href="/auth" className="flex flex-col w-16 text-center relative">
+      <Link href="/" className="flex flex-col w-16 text-center relative">
         <div className={cs('absolute', style.BackgroundCircle)} />
-        <FaHome size={38} className="shrink-0 ml-auto mr-auto" />
-        <span className={cs(`font-medium text-sm`)}>홈</span>
+        <HiHome size={28} className="shrink-0 ml-auto mr-auto" />
+        <span className={cs(`text-sm mt-1 font-bold`)}>홈</span>
       </Link>
-      <Link href="/mypage" className="flex flex-col w-16 text-center">
-        <FaUserAlt size={32} className="shrink-0 ml-auto mr-auto" />
-        <span className={cs(`font-medium text-xs`)}>마이페이지</span>
+      <Link href="/mypage" className="flex flex-col w-17 text-center">
+        <FaUserAlt size={28} className="shrbg-lineink-0 ml-auto mr-auto" />
+        <span className={cs(`text-xs mt-1 font-bold`)}>마이페이지</span>
       </Link>
     </MainFooter>
   );
