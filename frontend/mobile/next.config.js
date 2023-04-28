2d9a6d1ec27398f121e757f928ce8f893bd60c25
pthwan27 <parkssuu@gmail.com>
2023-04-28 14:29:55 +0900

    fix: next config수정

--- frontend/mobile/next.config.js
+++ frontend/mobile/next.config.js
@@ -1,9 +1,4 @@
 const runtimeCaching = require('next-pwa/cache');
-<<<<<<< HEAD
-
-=======
-// eslint-disable-next-line import/order
->>>>>>> 3d3e07cf1652d39e1436a6dc636ab0a1fac41af2
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
