350e49e85c1f593b8fe1d999af2d78474be46025
geonhyeon <geon1120@gmail.com>
2023-04-29 20:03:17 +0900

    refactor: 사업자등록번호검증url yml로 이동

--- backend/store-service/src/main/java/com/tandamzi/storeservice/service/ValidationService.java
+++ backend/store-service/src/main/java/com/tandamzi/storeservice/service/ValidationService.java
@@ -5,11 +5,12 @@ import com.tandamzi.storeservice.dto.request.BusinessValidationRequestDto;
 import com.tandamzi.storeservice.dto.response.BusinessValidationResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -21,10 +22,13 @@ import java.util.Map;
 @RequiredArgsConstructor
 @Slf4j
 public class ValidationService {
-    public boolean isValidBusinessLicense(BusinessValidationRequestDto requestDto) throws UnsupportedEncodingException, URISyntaxException {
-        String url = "http://api.odcloud.kr/api/nts-businessman/v1/validate?serviceKey=Xz1aVHGtadYwCQ4N9ymIHpP%2FIThGVuTnlj3X0Mhnd%2FeElMoR%2BZgsO1%2BPpQ86D20WCNO7WdzEN8CGBh1hL50rRA%3D%3D";
+    @Value("${public-api.business.url}")
+    private String url;
+
+    public boolean isValidBusinessLicense(BusinessValidationRequestDto requestDto) throws URISyntaxException {
         URI uri = new URI(url);
-        log.info("uri: {}", uri.toString());
+        log.info("url: {}", url);
+        log.info("uri: {}", uri);
 
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
@@ -37,12 +41,12 @@ public class ValidationService {
         ObjectMapper objectMapper = new ObjectMapper();
         BusinessValidationResponseDto responseDto = objectMapper.convertValue(resultMap.getBody(), BusinessValidationResponseDto.class);
         String valid = responseDto.getData().get(0).getValid();
+
         if (valid.equals("02")) {
             return false;
         }
         return true;
     }
-
 
 
 }
