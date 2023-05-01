dd2f8ffa55cc4a8edf11cba76936c7a0d04aa8d9
ss0takk <ss0takk@gmail.com>
2023-05-01 23:57:54 +0900

    feat: tag 수 조회

--- backend/review-service/src/main/java/com/tandamzi/reviewservice/repository/ReviewRepository.java
+++ backend/review-service/src/main/java/com/tandamzi/reviewservice/repository/ReviewRepository.java
@@ -6,7 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    List<Review> findByStoreId(@Param("storeId") Long storeId);
 
     Page<Review> findPageByStoreId(@Param("storeId") Long storeId, Pageable pageable);
 
