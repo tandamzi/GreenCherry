be2ce6794c417e100bbe1def111bd17cb862bb27
geonhyeon <geon1120@gmail.com>
2023-05-01 09:45:58 +0900

    feat: 사업자허가조회

--- backend/store-service/src/main/java/com/tandamzi/storeservice/service/ValidationService.java
+++ backend/store-service/src/main/java/com/tandamzi/storeservice/service/ValidationService.java
@@ -3,33 +3,36 @@ package com.tandamzi.storeservice.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tandamzi.storeservice.dto.request.BusinessValidationRequestDto;
 import com.tandamzi.storeservice.dto.response.BusinessValidationResponseDto;
+import com.tandamzi.storeservice.dto.response.PermissionValidationApiResponseDto;
+import com.tandamzi.storeservice.dto.response.PermissionValidationResponseDto;
+import com.tandamzi.storeservice.exception.BusinessLicenseNotValidException;
+import com.tandamzi.storeservice.exception.BusinessPermissionNotValidException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class ValidationService {
     @Value("${public-api.business.url}")
-    private String url;
+    private String businessUrl;
+    @Value(("${public-api.permission.url}"))
+    private String permissionUrl;
+
 
     public boolean isValidBusinessLicense(BusinessValidationRequestDto requestDto) throws URISyntaxException {
-        URI uri = new URI(url);
-        log.info("url: {}", url);
-        log.info("uri: {}", uri);
-
+        URI uri = new URI(businessUrl);
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -41,12 +44,37 @@ public class ValidationService {
         ObjectMapper objectMapper = new ObjectMapper();
         BusinessValidationResponseDto responseDto = objectMapper.convertValue(resultMap.getBody(), BusinessValidationResponseDto.class);
         String valid = responseDto.getData().get(0).getValid();
+        log.info("url: {}", businessUrl);
+        log.info("uri: {}", uri);
 
         if (valid.equals("02")) {
-            return false;
+            throw new BusinessLicenseNotValidException();
         }
         return true;
     }
 
 
+    public PermissionValidationApiResponseDto isValidBusinessPermission(String businessLicenseNumber) {
+        UriComponents uriComponent = UriComponentsBuilder
+                .fromHttpUrl(permissionUrl)
+                .path("/LCNS_NO=")
+                .path(businessLicenseNumber)
+                .build();
+        RestTemplate restTemplate = new RestTemplate();
+        ObjectMapper objectMapper = new ObjectMapper();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        ParameterizedTypeReference<Map<String, Object>> responseType = new ParameterizedTypeReference<Map<String, Object>>() {};
+        ResponseEntity<Map<String, Object>> responseEntity = restTemplate.exchange(uriComponent.toUriString(), HttpMethod.GET, null, responseType);
+        Map<String, Object> responseBodyMap = responseEntity.getBody();
+        Map<String, Object> responseTotalData = (Map<String, Object>) responseBodyMap.get("I2500");
+        List<Map<String, Object>> rows = (List<Map<String, Object>>) responseTotalData.get("row");
+        String totalCount = (String) responseTotalData.get("total_count");
+        if (!totalCount.equals("1")) {
+            throw new BusinessPermissionNotValidException();
+        }
+        Map<String, Object> firstRow = rows.get(0);
+        PermissionValidationApiResponseDto response = objectMapper.convertValue(firstRow, PermissionValidationApiResponseDto.class);
+        return response;
+    }
 }
