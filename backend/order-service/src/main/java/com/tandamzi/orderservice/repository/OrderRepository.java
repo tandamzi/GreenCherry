d0589a3d1203af114d74cd3f55191bfc19519d7b
tabss2003 <tabss2003@naver.com>
2023-05-01 17:52:11 +0900

    feat: 주문목록 service 구현

--- backend/order-service/src/main/java/com/tandamzi/orderservice/repository/OrderRepository.java
+++ backend/order-service/src/main/java/com/tandamzi/orderservice/repository/OrderRepository.java
@@ -1,10 +1,10 @@
 package com.tandamzi.orderservice.repository;
 
 import com.tandamzi.orderservice.domain.Order;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order ,Long> {
-
+public interface OrderRepository extends JpaRepository<Order ,Long> , OrderRepositoryCustom{
 }
