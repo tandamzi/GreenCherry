9c3640220d1f5aa390f3a9ce9e1ccf8e1df35041
DESKTOP-KVCQHCD\multicampus <codms1104@naver.com>
2023-05-01 13:10:36 +0900

    design: modal창 띄우기

--- frontend/web/src/components/Container/components/MainBody/index.jsx
+++ frontend/web/src/components/Container/components/MainBody/index.jsx
@@ -4,7 +4,10 @@ import Body from '@/components/Container/components/Body';
 
 const MainBody = ({ className, children, ref }) => {
   return (
-    <Body ref={ref} className={classnames('px-32', 'py-7', className)}>
+    <Body
+      ref={ref}
+      className={classnames('px-64', 'py-32', 'h-screen', className)}
+    >
       {children}
     </Body>
   );
--- frontend/web/src/pages/business/index.jsx
+++ frontend/web/src/pages/business/index.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Container from '@/components/Container';
+import MypageModal from '@/components/MypageModal';
 
 const Business = () => {
-  return <div>안녕</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpen = () => {
+    console.log(isOpen);
+    setIsOpen(true);
+  };
+  const handleClose = () => {
+    console.log(isOpen);
+    setIsOpen(false);
+  };
+
+  return (
+    <Container>
+      <Container.BusinessHeader />
+      <Container.MainBody className="bg-bgcolor">
+        <button type="button" onClick={handleOpen}>
+          모달창 열기
+        </button>
+        <MypageModal isOpen={isOpen} onClose={handleClose} />
+      </Container.MainBody>
+    </Container>
+  );
 };
 
 export default Business;
