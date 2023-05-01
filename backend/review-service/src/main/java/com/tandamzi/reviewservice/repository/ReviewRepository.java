a8a92acaa88c5cfb67ab6df78990369dac2ac59e
ss0takk <ss0takk@gmail.com>
2023-05-01 22:51:38 +0900

    feat: review 목록 조회

--- backend/review-service/src/main/java/com/tandamzi/reviewservice/repository/ReviewRepository.java
+++ backend/review-service/src/main/java/com/tandamzi/reviewservice/repository/ReviewRepository.java
@@ -1,8 +1,13 @@
 package com.tandamzi.reviewservice.repository;
 
 import com.tandamzi.reviewservice.domain.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findPageByStoreId(@Param("storeId") Long storeId, Pageable pageable);
 
 }
