21326182d1d49bba05ae46d4a4c6ab5c8f68fbf6
DESKTOP-KVCQHCD\multicampus <codms1104@naver.com>
2023-05-01 17:52:32 +0900

    desigb: 모달 상태

--- frontend/web/src/components/MypageModal/index.jsx
+++ frontend/web/src/components/MypageModal/index.jsx
@@ -2,19 +2,45 @@ import React from 'react';
 
 import classnames from 'classnames';
 
-import style from './index.module.scss';
-
 import Modal from '@/components/Modal';
+import StoreModify from '@/components/StoreModify';
 
 const MypageModal = ({ isOpen, onClose }) => {
+  const modify = {
+    state: false,
+    type: 'description',
+  };
+  const data = {
+    discription: '안녕아녕',
+    cherryDescription: '메롱',
+  };
+  const category = [
+    { title: '가게 소개글', type: 'description' },
+    { title: '체리박스 소개글', type: 'cherryDescription' },
+  ];
+
+  const handleChange = () => {
+    // console.log('handleChange');
+  };
   return (
     <Modal
       isOpen={isOpen}
       onClose={onClose}
-      className={classnames('w-2/3', 'h-5/6')}
+      className={classnames('w-2/3', 'h-5/6', 'px-40', 'py-11')}
     >
-      <h2>모달창 제목</h2>
-      <p>모달창 내용이 들어가는 부분입니다.</p>
+      <h2 className="text-center text-4xl">내 가게</h2>
+      <div>
+        {category.map(item => {
+          // type === 'allergy'&& return(div);
+          return (
+            <StoreModify title={item.title} type={item.type}>
+              {modify.state && item.type === modify.type && (
+                <textarea onChange={handleChange} />
+              )}
+            </StoreModify>
+          );
+        })}
+      </div>
     </Modal>
   );
 };
