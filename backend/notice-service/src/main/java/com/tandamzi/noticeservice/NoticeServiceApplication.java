a6ab6295fbfc647ecf1fbfc2292769ce7a154d90
ss0takk <ss0takk@gmail.com>
2023-05-02 23:38:54 +0900

    init: notice-service 생성

---
+++ backend/notice-service/src/main/java/com/tandamzi/noticeservice/NoticeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tandamzi.noticeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeServiceApplication.class, args);
+	}
+
+}
