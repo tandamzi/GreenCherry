771d17131c3c8d3bd7a59b7c3849ae51b4df0c3e
DESKTOP-KVCQHCD\multicampus <codms1104@naver.com>
2023-05-03 11:26:30 +0900

    design: 사업자 등록번호

--- frontend/web/src/pages/business/join/index.jsx
+++ frontend/web/src/pages/business/join/index.jsx
@@ -6,7 +6,13 @@ import style from './index.module.scss';
 import Container from '@/components/Container';
 
 const Join = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    watch,
+    reset,
+  } = useForm({ mode: 'onBlur' });
   return (
     <Container>
       <Container.MainHeader />
@@ -19,27 +25,90 @@ const Join = () => {
             </h3>
           </div>
           <div className="flex flex-col items-center">
-            <div className="relative">
+            <div className="relative my-5 border-b-2 border-bgcolor">
               <input
-                {...register('email', {
-                  required: {
-                    value: true,
-                    message: '이메일을 입력해주세요.',
-                  },
+                {...register('name', {
+                  required: true,
                 })}
                 autoComplete="off"
-                id="email"
+                id="name"
                 type="text"
-                className="text-2xl peer placeholder-transparent h-10 w-96 border-b-2 border-bgcolor bg-secondary text-bgcolor focus:outline-none"
-                placeholder="가게 이름"
+                className="text-2xl peer placeholder-transparent h-10 w-96  bg-secondary text-bgcolor focus:outline-none placeholder-secondary"
+                placeholder="상호명"
               />
               <label
-                htmlFor="email"
-                className="text-2xl absolute left-0 -top-3.5 text-bgcolor peer-placeholder-shown:text-2xl peer-placeholder-shown:text-bgcolor peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-bgcolor peer-focus:text-base"
+                htmlFor="name"
+                className="text-base absolute left-0 -top-3.5 text-bgcolor peer-placeholder-shown:text-2xl peer-placeholder-shown:text-bgcolor peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-bgcolor peer-focus:text-base"
               >
-                가게 이름
+                상호명
               </label>
             </div>
+            {errors.name && errors.name.type === 'required' && (
+              <p className={style['error-text']}>상호명을 입력해주세요</p>
+            )}
+            <div className="relative my-5 border-b-2 border-bgcolor w-96 flex">
+              <div className="w-80 overflow-hidden">
+                <input
+                  {...register('businessLicenseNumber', {
+                    required: true,
+                    pattern: /^[0-9]{3}-[0-9]{2}-[0-9]{5}$/,
+                  })}
+                  autoComplete="off"
+                  id="businessLicenseNumber"
+                  type="text"
+                  className="text-2xl peer placeholder-transparent h-10 bg-secondary text-bgcolor focus:outline-none placeholder-secondary"
+                  placeholder="사업자 등록 번호"
+                />
+                <label
+                  htmlFor="businessLicenseNumber"
+                  className="text-base absolute left-0 -top-3.5 text-bgcolor peer-placeholder-shown:text-2xl peer-placeholder-shown:text-bgcolor peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-bgcolor peer-focus:text-base"
+                >
+                  사업자 등록 번호
+                </label>
+              </div>
+              <button type="button" className={`${style['check-button']}`}>
+                확인하기
+              </button>
+            </div>
+            {errors.businessLicenseNumber &&
+              errors.businessLicenseNumber.type === 'required' && (
+                <p className={style['error-text']}>
+                  사업자 등록 번호를 입력해주세요
+                </p>
+              )}
+            {errors.businessLicenseNumber &&
+              errors.businessLicenseNumber.type === 'pattern' && (
+                <p className={style['error-text']}>
+                  사업자 등록 번호를 확인해주세요
+                </p>
+              )}
+            <div className="relative my-5 border-b-2 border-bgcolor">
+              <input
+                {...register('ownerName', {
+                  required: true,
+                  pattern: /^[가-힣]{2,10}$/,
+                })}
+                autoComplete="off"
+                id="ownerName"
+                type="text"
+                className="text-2xl peer placeholder-transparent h-10 w-96 bg-secondary text-bgcolor focus:outline-none placeholder-secondary"
+                placeholder="대표자 성명"
+              />
+              <label
+                htmlFor="ownerName"
+                className="text-base absolute left-0 -top-3.5 text-bgcolor peer-placeholder-shown:text-2xl peer-placeholder-shown:text-bgcolor peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-bgcolor peer-focus:text-base"
+              >
+                대표자 성명
+              </label>
+            </div>
+            {errors.ownerName && errors.ownerName.type === 'required' && (
+              <p className={style['error-text']}>대표자 성명을 입력해주세요</p>
+            )}
+            {errors.ownerName && errors.ownerName.type === 'pattern' && (
+              <p className={style['error-text']}>
+                대표자 성명은 2~10자 한글로 입력해주세요
+              </p>
+            )}
           </div>
         </div>
       </Container.MainBody>
