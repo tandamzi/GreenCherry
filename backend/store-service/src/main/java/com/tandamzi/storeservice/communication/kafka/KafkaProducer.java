60d96ebdc9e77a8c7bac4588c0a78f9a0b842138
geonhyeon <geon1120@gmail.com>
2023-05-03 16:24:27 +0900

    feat: kafka 설정추가

---
+++ backend/store-service/src/main/java/com/tandamzi/storeservice/communication/kafka/KafkaProducer.java
@@ -0,0 +1,15 @@
+package com.tandamzi.storeservice.communication.kafka;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class KafkaProducer {
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+
+}
