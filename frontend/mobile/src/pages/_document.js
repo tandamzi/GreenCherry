9ea39ec7f7dd757f3fbbe43bc7894d84eae5659a
pthwan27 <parkssuu@gmail.com>
2023-05-03 09:19:42 +0900

    feat: 지도추가, main 수정

--- frontend/mobile/src/pages/_document.js
+++ frontend/mobile/src/pages/_document.js
@@ -8,13 +8,15 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon-192x192.png" />
           <meta name="theme-color" content="#84A59D" />
-          {/* <meta name="viewport" content="width=device-width,initial-scale=1" /> */}
+          <script
+            type="text/javascript"
+            src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}`}
+            async
+          />
         </Head>
-        <body>
-          <div className="max-h-max max-w-md mx-auto">
-            <Main />
-            <NextScript />
-          </div>
+        <body className="absolute h-full w-full">
+          <Main />
+          <NextScript />
         </body>
       </Html>
     );
--- frontend/mobile/src/pages/index.jsx
+++ frontend/mobile/src/pages/index.jsx
@@ -70,45 +70,29 @@ export default function Home() {
     <Container>
       <Container.MainHeader />
       <Container.Body>
-        <Lottie
-          className="absolute top-80 ml-10 mt-4"
-          loop
-          animationData={reservation}
-          play
-          style={{ width: 230, height: 250 }}
-          speed={1}
-        />
         <div className="grid grid-rows-8 ">
           <div className="row-span-3">
             <MainCarbon />
           </div>
           <div className="row-span-2 grid grid-cols-2 justify-items-center">
             <Link href="/order">
-              <div className="relative">
+              <div>
                 <Image
-                  src="/assets/icons/selectBoxIcons/orderBox.svg"
-                  width={180}
-                  height={180}
+                  src="/assets/icons/selectBoxIcons/orderBoxInText.svg"
+                  width={280}
+                  height={280}
                   alt="greencherry orderBox"
                 />
-                <p className="absolute top-6 left-6 font-bold">주문</p>
-                <p className="absolute top-12 left-6 text-xs">
-                  주변 체리박스를
-                </p>
-                <p className="absolute top-16 left-6 text-xs">주문해보세요</p>
               </div>
             </Link>
             <Link href="/subscribe">
-              <div className="relative">
+              <div>
                 <Image
-                  src="/assets/icons/selectBoxIcons/subscribeBox.svg"
-                  width={180}
-                  height={180}
+                  src="/assets/icons/selectBoxIcons/subscribeBoxInText.svg"
+                  width={280}
+                  height={280}
                   alt="greencherry subscribeBox"
                 />
-                <p className="absolute top-6 left-6 font-bold">구독</p>
-                <p className="absolute top-12 left-6 text-xs">구독한 가게를</p>
-                <p className="absolute top-16 left-6 text-xs">볼 수 있어요</p>
               </div>
             </Link>
           </div>
