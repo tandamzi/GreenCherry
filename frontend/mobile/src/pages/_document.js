6d2bc8adf74568b733ff0022652a75d888200dce
pthwan27 <parkssuu@gmail.com>
2023-05-03 09:21:17 +0900

    chore: document src 수정

--- frontend/mobile/src/pages/_document.js
+++ frontend/mobile/src/pages/_document.js
@@ -11,7 +11,6 @@ class MyDocument extends Document {
           <script
             type="text/javascript"
             src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}`}
-            async
           />
         </Head>
         <body className="absolute h-full w-full">
