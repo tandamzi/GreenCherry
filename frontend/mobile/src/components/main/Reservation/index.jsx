a713d2f8556b802cefa7aa676d09e717de492d8b
pthwan27 <parkssuu@gmail.com>
2023-05-03 09:19:42 +0900

    feat: 지도추가, main 수정

--- frontend/mobile/src/components/main/Reservation/index.jsx
+++ frontend/mobile/src/components/main/Reservation/index.jsx
@@ -4,14 +4,33 @@ import reservation from '@public/assets/lottie/reservation1.json';
 import sprout1 from '@public/assets/lottie/sprout1.json';
 import cs from 'classnames';
 
-const Reservation = ({ className, children }) => {
+import style from './index.module.scss';
+
+const Reservation = () => {
+  const options = {
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid meet', // 애니메이션의 종횡비 유지
+    },
+  };
   return (
-    <div className=" bg-itembg grid grid-cols-3 h-36 mb-2 mx-2 rounded-xl shadow-lg">
-      <div className=" col-start-2 col-span-2 grid justify-items-center content-center">
-        <div className=" pl-4 grid justify-items-center content-center">
-          <p className=" text-base mb-2">진행중인 예약이 없습니다</p>
-        </div>
-      </div>
+    <div
+      className={cs(
+        style.reservationLottie,
+        'flex justify-start items-center p-4 bg-itembg mb-2 mx-2 rounded-xl shadow-lg',
+      )}
+    >
+      <Lottie
+        className="ml-10 mb-11"
+        loop
+        animationData={reservation}
+        play
+        options={options}
+        style={{ width: 260, height: 260 }}
+        speed={1}
+      />
+      <p className="relative right-10 text-base w-full mb-2">
+        진행중인 예약이 없습니다
+      </p>
     </div>
   );
 };
