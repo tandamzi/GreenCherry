9e9d28d48232279727b74882bc2641c07fd83225
pthwan27 <parkssuu@gmail.com>
2023-05-08 15:46:37 +0900

    Chore :주변 가게 정보 가져오기

--- frontend/mobile/src/server/api/http.js
+++ frontend/mobile/src/server/api/http.js
@@ -6,6 +6,8 @@ const http = axios.create({
   baseURL: 'http://k8C207.p.ssafy.io:5000',
   headers: {
     'Content-Type': 'application/json;charset=utf-8',
+    Authorization:
+      'Bearer JhbGciOiJIUzUxMiJ9.eyJzdWIiOiI5IiwiaWF0IjoxNjgzNTI2Mzk5LCJleHAiOjE2OTIxNjYzOTl9.xdKP9z53RZpCTlQSlykP126u-zy0fBNvqmF9BKBY2EL47SjffG48fbgzB9MIO_3b1slSv2PKErPBmy8eyVj3Ew',
   },
 });
 
