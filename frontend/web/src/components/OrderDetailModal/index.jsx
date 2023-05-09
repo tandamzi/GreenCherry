2040c3ab30b2c255514475098b7a537ac058a013
DESKTOP-KVCQHCD\multicampus <codms1104@naver.com>
2023-05-09 13:28:48 +0900

    design: OrderDetailModal

--- frontend/web/src/components/OrderDetailModal/index.jsx
+++ frontend/web/src/components/OrderDetailModal/index.jsx
@@ -5,13 +5,27 @@ import InfoModal from '@/components/InfoModal';
 const OrderDetailModal = ({ isOpen, onClose, order }) => {
   return (
     <InfoModal isOpen={isOpen} onClose={onClose}>
-      <p>주문상세</p>
-      <p>주문자: {order.nickname}</p>
+      <p className="text-3xl font-sans mb-6 mt-4">주문상세</p>
+      <div className="flex text-left text-2xl items-center">
+        <p className="flex-1 px-4 py-1 fl">주문자</p>
+        <p className="flex-1 px-4 py-1 font-sans">{order.nickname}</p>
+      </div>
+      <div className="flex text-left text-2xl">
+        <p className="flex-1 px-4 py-1 fl">수량</p>
+        <p className="flex-1 px-4 py-1 font-sans">{order.quantity}</p>
+      </div>
+      <div className="flex text-left text-2xl">
+        <p className="px-4 py-1 fl">주문상태</p>
+        <p className="px-4 py-1 font-sans">
+          {order.orderState === 'PICKUP_COMPLETE' ? ' 픽업완료' : ' 픽업대기'}
+        </p>
+      </div>
+      {/*       <p>주문자: {order.nickname}</p>
       <p>수량: {order.quantity}</p>
       <p>
         주문상태:
-        {order.orderState === 'PICKUP_COMPLETE' ? '픽업완료' : '픽업대기'}
-      </p>
+        {order.orderState === 'PICKUP_COMPLETE' ? ' 픽업완료' : ' 픽업대기'}
+      </p> */}
     </InfoModal>
   );
 };
--- frontend/web/src/components/PickUpCompleteModal/index.jsx
+++ frontend/web/src/components/PickUpCompleteModal/index.jsx
@@ -5,8 +5,10 @@ import InfoModal from '@/components/InfoModal';
 const PickUpCompleteModal = ({ isOpen, onClose, onClick, order }) => {
   return (
     <InfoModal isOpen={isOpen} onClose={onClose} onClick={onClick} okBtn>
-      <p>픽업 완료 상태로 바꾸시겠습니까?</p>
-      <p>주문번호: {order.orderId}</p>
+      <p className="text-3xl font-sans mb-6 mt-4">
+        픽업 완료 상태로 바꾸시겠습니까?
+      </p>
+      <p className="mb-4">주문번호: {order.orderId}</p>
       <p>주문자: {order.nickname}</p>
     </InfoModal>
   );
