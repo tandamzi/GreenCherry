d47fbd600772c2604741a1d4e137d93904f449f2
DESKTOP-KVCQHCD\multicampus <codms1104@naver.com>
2023-05-09 10:43:31 +0900

    design: main container 위치 수정

--- frontend/web/src/components/Container/components/MainBody/index.jsx
+++ frontend/web/src/components/Container/components/MainBody/index.jsx
@@ -6,10 +6,7 @@ const MainBody = ({ className, children, ref }) => {
   return (
     <Body
       ref={ref}
-      className={classnames(
-        'flex flex-col items-center py-32 w-fill m-auto max-w-4xl',
-        className,
-      )}
+      className={classnames('flex flex-col items-center py-32', className)}
     >
       {children}
     </Body>
