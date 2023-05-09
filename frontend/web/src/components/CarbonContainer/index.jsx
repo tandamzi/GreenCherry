3ea6302227a554dedbcb0f1f6d27886a147c2550
DESKTOP-KVCQHCD\multicampus <codms1104@naver.com>
2023-05-09 16:53:27 +0900

    design: 탄소

---
+++ frontend/web/src/components/CarbonContainer/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import classnames from 'classnames';
+
+import useMember from '@/hooks/memberHook';
+
+const CarbonContainer = ({ amount, className }) => {
+  const { storeName } = useMember();
+  return (
+    <div
+      className={classnames(
+        'bg-secondary opacity-50 text-bgcolor w-full text-3xl text-center rounded-full py-2 shadow-second',
+        className,
+      )}
+    >
+      {storeName} 가게는 {amount}Kg CO<sub>2</sub>e 만큼 줄였어요!
+    </div>
+  );
+};
+
+export default CarbonContainer;
