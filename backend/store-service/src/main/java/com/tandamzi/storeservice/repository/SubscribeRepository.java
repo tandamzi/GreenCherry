9b5f1ecc3a498d66865d4550ae0b5d5ca7d028dc
geonhyeon <geon1120@gmail.com>
2023-05-09 10:03:04 +0900

    feat: 구독가게목록

--- backend/store-service/src/main/java/com/tandamzi/storeservice/repository/SubscribeRepository.java
+++ backend/store-service/src/main/java/com/tandamzi/storeservice/repository/SubscribeRepository.java
@@ -2,6 +2,8 @@ package com.tandamzi.storeservice.repository;
 
 import com.tandamzi.storeservice.domain.Store;
 import com.tandamzi.storeservice.domain.Subscribe;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +15,6 @@ public interface SubscribeRepository extends JpaRepository<Subscribe, Long> {
     void deleteByStoreAndMemberId(Store store, Long memberId);
     List<Subscribe> findAllByStore(Store store);
     Long countByStoreId(Long storeId);
+    Page<Subscribe> findAllByMemberId(Long memberId, Pageable pageable);
+
 }
