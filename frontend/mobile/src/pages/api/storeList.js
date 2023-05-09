069340b2fc077aadd5f8d203ee2a034a3d63dfb1
pthwan27 <parkssuu@gmail.com>
2023-05-10 01:11:09 +0900

    fix : http 수정수정수정

--- frontend/mobile/src/pages/api/storeList.js
+++ frontend/mobile/src/pages/api/storeList.js
@@ -3,8 +3,9 @@ import { StatusCodes } from 'http-status-codes';
 import { storeListFetch } from '@/server/store/storeList';
 
 const handler = async (req, res) => {
-  // console.log('FE pages/api/SotreList');
   const { memberId, lat, lng, radius, sub } = req.query;
+  // console.log('\n FE pages/api/SotreList');
+  // console.log(req.query);
   try {
     const storeList = await storeListFetch.getStoreList(
       memberId,
@@ -13,6 +14,7 @@ const handler = async (req, res) => {
       radius,
       sub,
     );
+    // console.log('\n storeList' + storeList);
     res.status(StatusCodes.OK).json(storeList);
   } catch (error) {
     console.error('Error while fetching store list:', error);
