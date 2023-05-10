ca5c0b7d92cd218f92f2fbd40192b4e97aa1ef2f
DESKTOP-KVCQHCD\multicampus <codms1104@naver.com>
2023-05-10 16:51:38 +0900

    feat: api 모듈화 설정

--- frontend/web/src/pages/api/member/index.js
+++ frontend/web/src/pages/api/member/index.js
@@ -2,18 +2,19 @@ import axios from 'axios';
 
 import createHttpInstance from '@/utils/http';
 
+// 멤버Id를 가져오는 API
 export default async function handler(req, res) {
   try {
-    // Spring 서버에 요청
     const http = createHttpInstance(req);
 
     const response = await http.get('/member');
-
-    // Spring 서버에서 받은 데이터를 클라이언트에 전달
-    res.status(200).json(response.data);
+    // console.log(response.data);
+    const data = {
+      memberId: response.data.data.id,
+    };
+    // console.log(data);
+    res.status(200).json(data);
   } catch (error) {
-    // 에러 처리
-    // console.log(error);
     res.status(500).json({ message: 'An error occurred' });
   }
 }
--- frontend/web/src/pages/business/redirect/index.jsx
+++ frontend/web/src/pages/business/redirect/index.jsx
@@ -18,6 +18,7 @@ const Redirect = () => {
     const url = router.asPath;
     const fetchToken = async () => {
       const token = extractTokenFromUrl(url);
+
       if (token) {
         // 서버에 토큰을 전달하여 쿠키로 설정
         const response = await fetch(`/api/set-token?token=${token}`);
@@ -26,13 +27,11 @@ const Redirect = () => {
           // 쿠키 설정 후 원하는 페이지로 리다이렉트
           router.push('/business');
         } else {
-          // 에러 처리
           // console.log('토큰이 있지만 오류남');
           router.push('/');
         }
       } else {
         // 토큰이 없는 경우 처리
-        // 예: 에러 페이지로 리다이렉트
         // console.log('토큰이 없다');
         router.push('/');
       }
