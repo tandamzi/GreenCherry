4973d3a8dd58a6bf47967df3737f33c3fe2ff2d2
tabss2003 <tabss2003@naver.com>
2023-05-10 10:26:24 +0900

    feat: 날짜별 수익 조회

--- backend/order-service/src/main/java/com/tandamzi/orderservice/repository/OrderRepository.java
+++ backend/order-service/src/main/java/com/tandamzi/orderservice/repository/OrderRepository.java
@@ -1,7 +1,6 @@
 package com.tandamzi.orderservice.repository;
 
 import com.tandamzi.orderservice.domain.Order;
-import com.tandamzi.orderservice.dto.jpa.OrderDateDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,11 +8,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
+import javax.persistence.Tuple;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order ,Long> , OrderRepositoryCustom {
@@ -24,14 +21,12 @@ public interface OrderRepository extends JpaRepository<Order ,Long> , OrderRepos
     @Query("select o from Order o where o.id in :orderId")
     List<Order> findListById(@Param("orderId") List<Long> orderId);
 
-    @Query(value ="select new com.tandamzi.orderservice.dto.jpa.OrderDateDto(count(*),sum (Order.totalSalesAmount)) from Order " +
-            "where date_format(o.createDate,'%Y-%m-%d') = :orderDate and Order.storeId = :storeId ",nativeQuery = true)
-    Optional<OrderDateDto> findByStoreIdAndCreateDate(@Param("storeId")Long storeId, @Param("orderDate")LocalDate orderDate);
+    @Query("select count(*) as count ,sum (o.totalSalesAmount) as totalAmount from Order o " +
+            "where o.storeId = :storeId and o.createDate between :startDateTime and :endDateTime")
+    Object[] findByStoreIdAndCreateDate(@Param("storeId")Long storeId, @Param("startDateTime")LocalDateTime startDateTime ,@Param("endDateTime")LocalDateTime endDateTime);
 
-//    @Query(value = "select new com.ta" +
-//            "count(*) , sum (Order.totalSalesAmount) from Order o where date_format(o.createDate,'%Y-%m-%d') = :orderDate " +
-//            " and o.storeId = :storeId",nativeQuery = true)
-//    Optional<OrderDateDto> findCountByStoreIdAndCreateDate(@Param("storeId")Long storeId, @Param("orderDate")LocalDate orderDate);
-
+    @Query("select count(*) as count ,sum (o.totalSalesAmount) as totalAmount from Order o " +
+            "where o.storeId = :storeId and o.createDate between :startDateTime and :endDateTime")
+    Tuple findTupleByStoreIdAndCreateDate(@Param("storeId")Long storeId, @Param("startDateTime")LocalDateTime startDateTime , @Param("endDateTime")LocalDateTime endDateTime);
 
 }
