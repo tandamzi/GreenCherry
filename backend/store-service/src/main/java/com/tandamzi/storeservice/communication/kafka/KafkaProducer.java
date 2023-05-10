b5f3f0a3c9438ff30a9f270d96416671c0962a4b
geonhyeon <geon1120@gmail.com>
2023-05-10 16:43:40 +0900

    feat: notification service로 kafka message 전송

--- backend/store-service/src/main/java/com/tandamzi/storeservice/communication/kafka/KafkaProducer.java
+++ backend/store-service/src/main/java/com/tandamzi/storeservice/communication/kafka/KafkaProducer.java
@@ -1,14 +1,30 @@
 package com.tandamzi.storeservice.communication.kafka;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class KafkaProducer {
     private KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
 
+    public void send(String topic, List<String> endpointList) {
+        log.info("kafka Producer send endPointList : " + endpointList);
+        String jsonData = "";
+        try {
+            jsonData = objectMapper.writeValueAsString(endpointList);
+        } catch (JsonProcessingException e) {
+            log.error("JSON으로 직렬화 하는 도중 에러 발생", e);
+        }
+        kafkaTemplate.send(topic, jsonData);
+    }
 }
