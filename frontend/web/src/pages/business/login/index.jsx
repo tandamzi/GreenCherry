6a7e6249fc601cc7ee1c4cbb7cafb56c2affa33e
DESKTOP-KVCQHCD\multicampus <codms1104@naver.com>
2023-05-10 11:56:30 +0900

    feat: a태그 Link로 변경

--- frontend/web/src/pages/business/login/index.jsx
+++ frontend/web/src/pages/business/login/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 import style from './index.module.scss';
 
 import Container from '@/components/Container';
@@ -8,29 +11,27 @@ const Login = () => {
   return (
     <Container>
       <Container.MainHeader />
-      <Container.MainBody className="bg-secondary">
+      <Container.MainBody className="bg-secondary h-full">
         <div className="text-bgcolor text-center mb-24">
           <h1 className="text-7xl mb-6">LOGIN INTO MYSTORE</h1>
           <h3 className="text-4xl font-thin">남은 음식을 수익으로 바꿔봐요</h3>
         </div>
         <div className={style['login-container']}>
-          <button
-            type="button"
-            // onClick={() => socialLogin('kakao')}
+          <Link
+            href={`http://k8C207.p.ssafy.io:5000/oauth2/authorize/kakao?redirect_uri=${process.env.NEXT_PUBLIC_OAUTH2_BUSINESS_REDIRECT_URI}`}
             className={`${style['kakao-btn']}`}
           >
             <div className={`${style['login-btn-img']}`} />
-            <p>카카오로 로그인하기</p>
-          </button>
+            카카오로 로그인하기
+          </Link>
 
-          <button
-            type="button"
-            // onClick={() => socialLogin('google')}
+          <Link
+            href={`${process.env.NEXT_PUBLIC_API_BASE_URL}/oauth2/authorize/google?redirect_uri=${process.env.NEXT_PUBLIC_OAUTH2_BUSINESS_REDIRECT_URI}`}
             className={`${style['google-btn']}`}
           >
             <div className={style['login-btn-img']} />
             <p>구글로 로그인하기</p>
-          </button>
+          </Link>
         </div>
       </Container.MainBody>
     </Container>
