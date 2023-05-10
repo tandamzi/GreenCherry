3029aaa3b7d8718b6e3b79bc4c61ab8f2706beb2
geonhyeon <geon1120@gmail.com>
2023-05-10 16:09:30 +0900

    refactor: kafka package 구조

--- backend/store-service/src/main/java/com/tandamzi/storeservice/communication/kafka/KafkaConsumerConfig.java
+++ backend/store-service/src/main/java/com/tandamzi/storeservice/communication/kafka/KafkaConsumerConfig.java
@@ -1,4 +1,4 @@
-package com.tandamzi.storeservice.kafka;
+package com.tandamzi.storeservice.communication.kafka;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
