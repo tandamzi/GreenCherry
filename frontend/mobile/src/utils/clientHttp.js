8c92aa3f163b2c492a1f8b5bc5a1d699a4965724
조성욱 <saeloun8797@gmail.com>
2023-05-11 14:05:20 +0900

    conflict 해결

---
+++ frontend/mobile/src/utils/clientHttp.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const LOCAL_API_URL = process.env.NEXT_PUBLIC_LOCAL_API_URL;
+
+const clientHttp = axios.create({
+  baseURL: LOCAL_API_URL,
+});
+
+export default clientHttp;
