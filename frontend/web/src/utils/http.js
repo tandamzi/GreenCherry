dcc797ad6fed0aef35faf078799dce9ff232f75a
DESKTOP-KVCQHCD\multicampus <codms1104@naver.com>
2023-05-11 15:13:17 +0900

    feat: join페이지 muti-part form submit 설정

--- frontend/web/src/utils/http.js
+++ frontend/web/src/utils/http.js
@@ -30,3 +30,32 @@ export default function createHttpInstance(req) {
 
   return instance;
 }
+
+export function createHttpFormInstance(req) {
+  const instance = axios.create({
+    baseURL: 'http://k8C207.p.ssafy.io:5000',
+    headers: { 'Content-Type': 'multipart/form-data' },
+  });
+
+  instance.interceptors.request.use(
+    async config => {
+      // 클라이언트의 쿠키에서 토큰을 가져옵니다.
+      const cookies = req.headers.cookie
+        ? parseCookies(req.headers.cookie)
+        : {};
+      const { token } = cookies;
+
+      if (token) {
+        // 헤더에 인증 토큰을 추가합니다.
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+
+      return config;
+    },
+    error => {
+      return Promise.reject(error);
+    },
+  );
+
+  return instance;
+}
