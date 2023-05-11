dc2e9652e7c3eac68e57060ecb9de0ae6f43a5a0
조성욱 <saeloun8797@gmail.com>
2023-05-11 14:05:20 +0900

    conflict 해결

--- frontend/mobile/src/pages/api/storeList.js
+++ frontend/mobile/src/pages/api/storeList.js
@@ -19,7 +19,7 @@ const handler = async (req, res) => {
 
     res.status(StatusCodes.OK).json(storeList);
   } catch (error) {
-    console.error('Error while fetching store list:', error);
+    // console.error('Error while fetching store list:', error);
     res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
       .json({ message: 'Internal server error' });
