eedfd70a13ebee28cf8f38fa415e21d6a14f4e5d
DESKTOP-KVCQHCD\multicampus <codms1104@naver.com>
2023-05-11 11:45:43 +0900

    feat: 로그인 후 join상태에 따른 redux 값 저장

--- frontend/web/src/pages/business/redirect/index.jsx
+++ frontend/web/src/pages/business/redirect/index.jsx
@@ -3,11 +3,13 @@ import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 import Container from '@/components/Container';
+import useStore from '@/hooks/memberHook';
 import { getMember } from '@/utils/api/member';
 import clientHttp from '@/utils/clientHttp';
 
 const Redirect = () => {
   const router = useRouter();
+  const { login, setMemberId } = useStore();
 
   const extractTokenFromUrl = url => {
     const urlParams = new URLSearchParams(url.split('?')[1]);
@@ -17,9 +19,13 @@ const Redirect = () => {
   const handleTokenResponse = async token => {
     if (token) {
       const response = await clientHttp.get(`/set-token?token=${token}`);
-      // TODO: redux에 회원, 상점정보 저장하기
       if (response.data.success) {
         const data = await getMember();
+        if (data.isJoined) {
+          login(data);
+        } else {
+          setMemberId(data);
+        }
         const destination = data.isJoined ? '/business' : '/business/join';
         router.push(destination);
       } else {
