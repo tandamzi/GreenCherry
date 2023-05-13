4470041a6f0a59056ce4184d9808ccc884739639
이채은 <codms1104@naver.ocm>
2023-05-14 02:03:53 +0900

    feat: 픽업완료로 상태 변경 api

--- frontend/web/src/pages/api/order/[orderId]/order-complete.js
+++ frontend/web/src/pages/api/order/[orderId]/order-complete.js
@@ -2,11 +2,11 @@ import createHttpInstance from '@/utils/http';
 
 export default async function handler(req, res) {
   const {
-    query: { storeId },
+    query: { orderId },
   } = req;
   try {
     const http = createHttpInstance(req);
-    const response = await http.put(`/store/${storeId}/toggle-store`);
+    const response = await http.put(`/order/${orderId}/ORDER_COMPLETE`);
     res.status(200).json(response.data);
   } catch (error) {
     res.status(500).json({ message: 'An error occurred' });
