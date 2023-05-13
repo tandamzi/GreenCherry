f41bd4a4c6e97aaede5cf4f536eb6b7631e2686d
이채은 <codms1104@naver.ocm>
2023-05-13 23:36:29 +0900

    design: root page 정리

--- frontend/web/src/pages/index.jsx
+++ frontend/web/src/pages/index.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useEffect, CSSProperties, useRef, useState } from 'react';
 
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-import style from './index.module.scss';
-
 import Container from '@/components/Container';
+import Section1 from '@/components/LandingPageComponent/Section1';
+import Section2 from '@/components/LandingPageComponent/Section2';
+import Section3 from '@/components/LandingPageComponent/Section3';
+import Section4 from '@/components/LandingPageComponent/Section4';
 
 export default function Home() {
   return (
@@ -14,58 +16,10 @@ export default function Home() {
       <Container.MainHeader />
       <Container.PaddingZeroBody className="bg-secondary block text-bgcolor">
         <div className="h-screen w-full overflow-y-scroll snap-y snap-mandatory">
-          <div className={`${style['section-item']}`}>
-            <h2 className={`${style['title-text']}`}>
-              행동을 <br />
-              통한 <br />
-              긍정적인 변화 <br />
-            </h2>
-            <p className={`${style['sub-text']}`}>
-              음식물 쓰레기로 인한 온실가스 배출량 <br />
-              <span className={`${style.emphasis}`}>
-                연간 885만톤 <br />
-              </span>
-              Green Cherry와 함께 지구를 지켜가요
-            </p>
-
-            <div className={`${style['image-container']}`}>
-              <div className={`${style['image-relative']}`}>
-                <Image
-                  src="/assets/images/money-growth.svg"
-                  width={156}
-                  height={156}
-                  alt="money-growth"
-                  className={`${style['money-growth']}`}
-                />
-                <Image
-                  src="/assets/images/love-ecology.svg"
-                  width={213}
-                  height={213}
-                  alt="love-ecology"
-                  className={`${style['love-ecology']}`}
-                />
-                <Image
-                  src="/assets/images/food-box.svg"
-                  width={498}
-                  height={498}
-                  alt="greencherry box"
-                  className={`${style['greencherry-box']}`}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="snap-always snap-center w-full h-screen bg-danger">
-            <p>2</p>
-          </div>
-          <div className="snap-always snap-center w-full h-screen bg-disabled">
-            <p>3</p>
-          </div>
-          <div className="snap-always snap-center w-full h-screen bg-warning">
-            <p>4</p>
-          </div>
-          <div className="snap-always snap-center w-full h-screen bg-primaryfont">
-            <p>5</p>
-          </div>
+          <Section1 />
+          <Section2 />
+          <Section3 />
+          <Section4 />
         </div>
       </Container.PaddingZeroBody>
     </Container>
