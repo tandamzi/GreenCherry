5f804e11c5e5bd1c4a6c1368c45da66fe479b1e9
이채은 <codms1104@naver.com>
2023-05-15 10:47:01 +0900

    fix: 네 누르면 요청가는 함수 바꿈

--- frontend/web/src/components/OrderListItem/index.jsx
+++ frontend/web/src/components/OrderListItem/index.jsx
@@ -53,7 +53,7 @@ const OrderListItem = ({ order, updateOrderState }) => {
       <PickUpCompleteModal
         isOpen={isPickUpCompleteModalOpen}
         onClose={handlePickUpCompleteModalClose}
-        onClick={handlePickUpCompleteModalOpen}
+        onClick={handlePickUpCompleteClick}
         order={order}
       />
 
