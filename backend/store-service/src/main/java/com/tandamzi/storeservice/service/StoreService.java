1f5675dd01b9be82caa1a66de1c453bade06dca3
geonhyeon <geon1120@gmail.com>
2023-05-15 10:40:21 +0900

    feat: 구독여부 조회API

--- backend/store-service/src/main/java/com/tandamzi/storeservice/service/StoreService.java
+++ backend/store-service/src/main/java/com/tandamzi/storeservice/service/StoreService.java
@@ -271,4 +271,8 @@ public class StoreService {
         Store store = storeRepository.findById(storeId).orElseThrow(StoreNotFoundException::new);
         return store.getCherryPoint();
     }
+
+    public Boolean isSubscribedStore(Long memberId, Long storeId) {
+        return subscribeRepository.findByMemberIdAndStoreId(memberId, storeId).isPresent();
+    }
 }
