d72a9c9ba78411bd3184243855e4e20966fd8df2
이채은 <codms1104@naver.ocm>
2023-05-16 01:48:36 +0900

    fix: cherryPoint 에러 처리 로직 추가

--- frontend/web/src/pages/business/index.jsx
+++ frontend/web/src/pages/business/index.jsx
@@ -11,11 +11,20 @@ const Business = () => {
   const { storeAttributes, setCherryPoint } = useStore();
   const { memberAttributes } = useMember();
 
-  useEffect(() => {
+  /*   useEffect(() => {
     getCherryPoint(memberAttributes.storeId)
       .then(data => setCherryPoint(data))
       .catch(error => console.error(error));
-  }, []);
+  }, []); */
+  useEffect(() => {
+    // console.log('getCherryPoint 실행');
+    getCherryPoint(memberAttributes.storeId)
+      .then(data => {
+        // console.log('getCherryPoint: data', data);
+        setCherryPoint(data);
+      })
+      .catch(error => console.error(error));
+  }, [memberAttributes.storeId]);
 
   return (
     <Container>
