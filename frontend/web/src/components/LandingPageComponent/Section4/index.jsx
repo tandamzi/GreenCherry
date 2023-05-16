3368aa01af70bd0df90ccd9da41b7b6c43f48b90
이채은 <codms1104@naver.com>
2023-05-17 02:23:43 +0900

    design: landing

--- frontend/web/src/components/LandingPageComponent/Section4/index.jsx
+++ frontend/web/src/components/LandingPageComponent/Section4/index.jsx
@@ -7,19 +7,6 @@ import style from './index.module.scss';
 const Section4 = () => {
   return (
     <div className={`${style['section-item']}`}>
-      <div>
-        <h2 className={`${style['title-text']}`}>
-          손해를 <br />
-          수익으로 <br />
-          전환해요! <br />
-        </h2>
-        <p className={`${style['sub-text']}`}>
-          <span className={`${style.emphasis}`}>
-            그렇군요 <br />
-          </span>
-          음식을 팔아 뭐라하지?
-        </p>
-      </div>
       <div className={`${style['image-container']}`}>
         <div className={`${style['image-relative']}`}>
           <Image
@@ -44,6 +31,15 @@ const Section4 = () => {
             className={`${style['money-bag']}`}
           />
         </div>
+      </div>
+      <div>
+        <p className={`${style['sub-text']}`}>
+          먹지않고 버려지는 <br />
+          음식의 경제가치
+        </p>
+        <h2 className={`${style['title-text']}`}>
+          약 <span className={`${style.emphasis}`}>3조</span>원
+        </h2>
       </div>
     </div>
   );
