3024bd25941e208dc2f1822613618ed31c2475b8
이채은 <codms1104@naver.com>
2023-05-17 14:20:53 +0900

    design: carboncontainer 모바일

--- frontend/web/src/components/OrderZero/index.jsx
+++ frontend/web/src/components/OrderZero/index.jsx
@@ -6,8 +6,8 @@ import CarbonCircleContainer from '@/components/CarbonCircleContainer';
 
 const OrderZero = () => {
   return (
-    <div className="mt-10">
-      <h2 className="text-center font-sans text-5xl tablet:text-4xl">
+    <div className="mt-5 flex flex-col items-center">
+      <h2 className="text-center font-sans text-5xl tablet:text-3xl">
         아직 주문이 안 들어왔어요!
       </h2>
       <div className="relative">
@@ -18,7 +18,7 @@ const OrderZero = () => {
           alt="greencherry box"
           className="mx-auto opacity-50 max-w-md width-full"
         />
-        <CarbonCircleContainer className="px-20 py-10 text-4xl leading-snug mt-5 tablet:px-10 tablet:text-3xl max-w-sm absolute top-40 left-1/2 -translate-x-1/2" />
+        <CarbonCircleContainer className="px-10 py-10 text-4xl leading-snug mt-5 tablet:px-10 tablet:text-3xl absolute top-40 w-11/12 -translate-x-1/2 left-1/2" />
       </div>
     </div>
   );
