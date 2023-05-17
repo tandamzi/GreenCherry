7c4c171d400838be57a8c83062b15da2a8ade5c4
조성욱 <saeloun8797@gmail.com>
2023-05-18 01:42:01 +0900

    feat: order-lit 예약현황 component 반영

--- frontend/mobile/src/components/OrderInfo/index.jsx
+++ frontend/mobile/src/components/OrderInfo/index.jsx
@@ -13,50 +13,78 @@ const OrderInfo = ({ orderInfo }) => {
   const navigate = useRouter();
 
   const newOrderDate = new Date(orderInfo.orderDate);
+  const { orderState } = orderInfo;
 
   return (
-    <div className="mb-3 border-b border-line">
-      <div className="m-3 flex flex-row justify-between">
-        <p>{newOrderDate.toLocaleDateString()}</p>
-        <p>{orderInfo.totalSalesAmount}원</p>
-      </div>
-      <div className="flex flex-row m-3">
-        <Image
-          src={orderInfo.storeImageUrl}
-          className="rounded-lg mr-3"
-          width={110}
-          height={60}
-          alt="reveiewImageUrl"
-        />
-        <div className="w-full">
-          <p className="font-bold text-2xl mt-3">{orderInfo.storeName}</p>
-          <p className="font-thin mb-2">체리박스 {orderInfo.quantity}</p>
-          {orderInfo.writed === 'EXPIRATION' ? (
-            <div className="text-center w-full py-1 rounded-lg border border-disabled">
-              <p className="text-disabled">리뷰 작성일이 지났습니다</p>
+    <div>
+      {orderState === 'ORDER_COMPLETE' ? (
+        <div className="mb-3 border-b border-line">
+          <div className="m-3 flex flex-row justify-between">
+            <p>{newOrderDate.toLocaleDateString()}</p>
+            <p>{orderInfo.totalSalesAmount}원</p>
+          </div>
+          <div className="flex flex-row m-3">
+            <Image
+              src={orderInfo.storeImageUrl}
+              className="rounded-lg mr-3"
+              width={110}
+              height={60}
+              alt="reveiewImageUrl"
+            />
+            <div className="w-full">
+              <p className="font-bold text-2xl mt-3">{orderInfo.storeName}</p>
+              <p className="font-thin mb-2">체리박스 {orderInfo.quantity}</p>
+              <div className="text-center w-full py-1 rounded-lg border border-primaryevent">
+                <p className="text-primaryevent">현재 예약중인 주문입니다!</p>
+              </div>
             </div>
-          ) : (
-            <div>
-              {orderInfo.writed === 'NO' ? (
-                <div
-                  className="text-center w-full bg-itembg py-1 rounded-lg active:bg-primary"
-                  onClick={() => {
-                    navigate.push(
-                      `review-form/${orderInfo.memberId}/${orderInfo.storeId}/${orderInfo.orderId}`,
-                    );
-                  }}
-                >
-                  <p>리뷰작성</p>
+          </div>
+        </div>
+      ) : (
+        <div className="mb-3 border-b border-line">
+          <div className="m-3 flex flex-row justify-between">
+            <p>{newOrderDate.toLocaleDateString()}</p>
+            <p>{orderInfo.totalSalesAmount}원</p>
+          </div>
+          <div className="flex flex-row m-3">
+            <Image
+              src={orderInfo.storeImageUrl}
+              className="rounded-lg mr-3"
+              width={110}
+              height={60}
+              alt="reveiewImageUrl"
+            />
+            <div className="w-full">
+              <p className="font-bold text-2xl mt-3">{orderInfo.storeName}</p>
+              <p className="font-thin mb-2">체리박스 {orderInfo.quantity}</p>
+              {orderInfo.writed === 'EXPIRATION' ? (
+                <div className="text-center w-full py-1 rounded-lg border border-disabled">
+                  <p className="text-disabled">리뷰 작성일이 지났습니다</p>
                 </div>
               ) : (
-                <div className="text-center w-full py-1 rounded-lg border border-disabled">
-                  <p className="text-disabled">이미 리뷰를 작성했습니다</p>
+                <div>
+                  {orderInfo.writed === 'NO' ? (
+                    <div
+                      className="text-center w-full bg-itembg py-1 rounded-lg active:bg-primary"
+                      onClick={() => {
+                        navigate.push(
+                          `review-form/${orderInfo.memberId}/${orderInfo.storeId}/${orderInfo.orderId}`,
+                        );
+                      }}
+                    >
+                      <p>리뷰작성</p>
+                    </div>
+                  ) : (
+                    <div className="text-center w-full py-1 rounded-lg border border-disabled">
+                      <p className="text-disabled">이미 리뷰를 작성했습니다</p>
+                    </div>
+                  )}
                 </div>
               )}
             </div>
-          )}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
