ad45e7c0056b6ab6870734f10bf3da0005d22318
이채은 <codms1104@naver.com>
2023-05-17 17:18:24 +0900

    fix: 리뷰페이지 오류

--- frontend/web/src/pages/api/store/[storeId]/review-count.js
+++ frontend/web/src/pages/api/store/[storeId]/review-count.js
@@ -7,7 +7,6 @@ export default async function handler(req, res) {
   try {
     const http = createHttpInstance(req);
     const response = await http.get(`/review/count?store-id=${storeId}`);
-
     res.status(200).json(response.data);
   } catch (error) {
     res.status(500).json({ message: 'An error occurred' });
