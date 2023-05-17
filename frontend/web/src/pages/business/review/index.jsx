7162c7cc8a3dd316a6f69f1aa4b5796fef2bc27e
이채은 <codms1104@naver.com>
2023-05-17 12:14:24 +0900

    feat: 리뷰 컴포넌트

--- frontend/web/src/pages/business/review/index.jsx
+++ frontend/web/src/pages/business/review/index.jsx
@@ -1,44 +1,64 @@
-import { useEffect, useState } from 'react';
-import Lottie from 'react-lottie-player';
+/* eslint-disable no-promise-executor-return */
+import React, { useEffect, useState } from 'react';
 
-import cs from 'classnames';
-import Image from 'next/image';
+import Container from '@/components/Container';
+import ReviewList from '@/components/ReviewList';
+import useMember from '@/hooks/memberHook';
+import { getReviewCount, getPagableReviewList } from '@/utils/api/review';
 
-import ReviewComponent from '@/components/ReviewComponent';
+const Review = () => {
+  const { memberAttributes } = useMember();
+  const [reviewList, setReviewList] = useState(null);
+  const [page, setPage] = useState(0);
+  const [pageEnd, setpageEnd] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [reviewCount, setReviewCount] = useState(0);
 
-const CRYING_ICON_URL = `/assets/icons/crying-face.svg`;
-const Review = ({ reviewInfo }) => {
-  const [data, setData] = useState();
   useEffect(() => {
-    setData(reviewInfo);
-  }, [reviewInfo]);
-  return (
-    <div>
-      <div className="flex flex-row justify-between items-center mx-8 mt-8">
-        <p className="font-bold text-xl pr-4">사용자 리뷰</p>
-        <span className=" text-sm align-text-bottom">
-          <span className="text-secondary">{data && data.totalElements}</span>건
-        </span>
-      </div>
-      {data && data.totalElements ? (
-        <div className=" w-10/12 mx-auto mt-5">
-          {reviewInfo.content.map((review, idx) => {
-            return (
-              <div key={idx}>
-                <ReviewComponent review={review} />
-                <div className="pb-4 border-line border-b -mx-8" />
-              </div>
-            );
-          })}
-        </div>
-      ) : (
-        <div className="flex justify-center items-center w-full text-2xl h-20">
-          <Image alt="no-review" src={CRYING_ICON_URL} width={32} height={32} />
+    getReviewCount(memberAttributes.storeId).then(res => {
+      setReviewCount(res.data);
+    });
+    getPagableReviewList(memberAttributes.storeId, page).then(res => {
+      setReviewList(res.reviewList);
+    });
+  }, []);
 
-          <span className="ml-1 pt-1"> 아직 작성된 리뷰가 없어요</span>
+  const loadMoreReviews = async () => {
+    if (pageEnd || isLoading) return;
+    setIsLoading(true);
+    const nextPage = page + 1;
+    getPagableReviewList(memberAttributes.storeId, nextPage)
+      .then(res => new Promise(resolve => setTimeout(() => resolve(res), 1000)))
+      .then(res => {
+        if (res.orderList.length === 0) {
+          setpageEnd(true);
+          return;
+        }
+        setReviewList(prevList => [...prevList, ...res.reviewList]);
+        setPage(prevPage => prevPage + 1);
+      })
+      .finally(() => setIsLoading(false)); // Stop loading
+  };
+
+  return (
+    <Container>
+      <Container.BusinessHeader />
+      <Container.MainBody className="bg-bgcolor h-full ">
+        <div className="flex items-center mx-8 mt-8">
+          <p className="font-bold text-xl pr-4">사용자 리뷰</p>
+          <span className=" text-sm align-text-bottom">
+            <span className="text-secondary">{reviewCount}</span>건
+          </span>
         </div>
-      )}
-    </div>
+        <div className="w-full h-full max-w-4xl relative">
+          <ReviewList
+            reviewList={reviewList}
+            reviewCount={reviewCount}
+            loadMoreReviews={loadMoreReviews}
+          />
+        </div>
+      </Container.MainBody>
+    </Container>
   );
 };
 
