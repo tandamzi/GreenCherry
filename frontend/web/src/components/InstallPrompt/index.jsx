d23ad5dce68b09f0865e748cb5fdef0975056678
이채은 <codms1104@naver.com>
2023-05-21 02:20:26 +0900

    chore: 문구 수정

--- frontend/web/src/components/InstallPrompt/index.jsx
+++ frontend/web/src/components/InstallPrompt/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
+import { CgSoftwareUpload } from 'react-icons/cg';
 
 import Image from 'next/image';
 
@@ -54,10 +55,23 @@ const InstallPrompt = () => {
       <div className={style.backdrop} />
       <div className={style['install-prompt']}>
         {isIOS ? (
-          <p>
-            Please use the &quot;Add to Home Screen&quot; option in your browser
-            menu to install this app.
-          </p>
+          <div className={style['install-container']}>
+            <Image
+              src="/assets/logo/cherryLogoShadowRemove.svg"
+              width={69}
+              height={69}
+              alt="greencherry main logo"
+            />
+            <div className="text-center">
+              <span className="text-secondary font-bold">GreenCherry</span>
+              는 앱에서 원활한 사용을 할 수 있습니다.
+              <br />
+            </div>
+            <div className="flex text-xl">
+              <CgSoftwareUpload size={24} />를 클릭하여 홈 화면에 추가하기를
+              통해 설치를 해주세요
+            </div>
+          </div>
         ) : (
           <div className={style['install-container']}>
             <Image
@@ -68,7 +82,7 @@ const InstallPrompt = () => {
             />
             <div className="text-center">
               <span className="text-secondary font-bold">GreenCherry</span>
-              는 앱을 지원합니다.
+              는 앱에서 원활한 사용을 할 수 있습니다.
               <br />
               <span className="text-xl">설치하시겠습니까?</span>
             </div>
