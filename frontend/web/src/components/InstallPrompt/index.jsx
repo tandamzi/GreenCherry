8ba62bc73ae7b24983bcc146bc50441203a59223
이채은 <codms1104@naver.com>
2023-05-21 21:59:58 +0900

    fix: ios에서 안닫히는 문제 수정

--- frontend/web/src/components/InstallPrompt/index.jsx
+++ frontend/web/src/components/InstallPrompt/index.jsx
@@ -8,18 +8,19 @@ import style from './index.module.scss';
 
 const InstallPrompt = () => {
   const [isShown, setIsShown] = useState(false);
-  const [isIOS, setIsIOS] = useState(false);
+  // const [isIOS, setIsIOS] = useState(false);
+  const isIOS = true;
   const [deferredPrompt, setDeferredPrompt] = useState(null);
 
   useEffect(() => {
     const isDeviceIOS =
       /iPad|iPhone|iPod/.test(window.navigator.userAgent) && !window.MSStream;
-    setIsIOS(isDeviceIOS);
+    // setIsIOS(isDeviceIOS);
 
     const handleBeforeInstallPrompt = e => {
       e.preventDefault();
       setDeferredPrompt(e);
-      setIsShown(true); // 'beforeinstallprompt' 이벤트가 발생했으므로 PWA가 설치되지 않았음을 알 수 있습니다.
+      setIsShown(true);
     };
 
     window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
@@ -43,11 +44,11 @@ const InstallPrompt = () => {
   };
 
   const handleClose = () => {
-    event.stopPropagation();
     setIsShown(false);
+    event.stopPropagation();
   };
 
-  if (!isIOS && !isShown) {
+  if (!isShown) {
     return null;
   }
 
@@ -73,10 +74,17 @@ const InstallPrompt = () => {
                 <CgSoftwareUpload size={24} />를 클릭하여 홈 화면에 추가하기를
                 통해 설치를 해주세요
               </div>
+              <button
+                type="button"
+                onClick={handleClose}
+                className={style['install-btn']}
+              >
+                확인
+              </button>
             </div>
             <button
               type="button"
-              onTouchEnd={handleClose}
+              onClick={handleClose}
               className={style['close-btn']}
             >
               <AiOutlineClose size={24} />
